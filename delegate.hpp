2f79ed1c756a7c1054b69497a69dd25c1574de79
Nikita Samusev <nikitf777@gmail.com>
2025-05-24 15:23:30 +0300

    Make Delegate take a type for the functions container as an argument

--- delegate.hpp
+++ delegate.hpp
@@ -1,48 +1,70 @@
+#include <deque>
 #include <functional>
+#include <list>
 #include <type_traits>
 #include <vector>
 
-template <typename T> class Delegate;
+// Primary template (undefined)
+template <typename T,
+		  template <typename...> class FunctionContainer = std::vector>
+class Delegate;
 
-template <typename Ret, typename... Args> class Delegate<Ret(Args...)> {
+// Specialization for function signature Ret(Args...)
+template <typename Ret, typename... Args,
+		  template <typename...> class FunctionContainer>
+class Delegate<Ret(Args...), FunctionContainer> {
   public:
 	using FunctionType = std::function<Ret(Args...)>;
 
-	template <typename T>
-	Delegate(T &&func) : functions({std::forward<T>(func)}) {}
+	// Constructor from a callable
+	template <typename TFunc>
+	Delegate(TFunc &&func) : functions({std::forward<TFunc>(func)}) {}
 
+	// Default constructors and assignment operators
 	Delegate() = default;
-
 	Delegate(const Delegate &) = default;
 	Delegate(Delegate &&) = default;
-
 	Delegate &operator=(const Delegate &) = default;
 	Delegate &operator=(Delegate &&) = default;
 
+	// Add a function via operator +=
 	void operator+=(FunctionType func) {
 		if (func)
 			functions.push_back(std::move(func));
 	}
 
+	// Add a function via method
 	void Add(FunctionType func) { *this += std::move(func); }
 
-	Ret operator()(Args... args) const {
+	// Call operator: invoke all bound functions
+	std::vector<Ret> operator()(Args... args) const {
 		if constexpr (std::is_void_v<Ret>) {
 			for (const auto &func : functions) {
 				func(std::forward<Args>(args)...);
 			}
 		} else {
-			Ret result{};
+			std::vector<Ret> results;
+			results.reserve(functions.size());
 			for (const auto &func : functions) {
-				result = func(std::forward<Args>(args)...);
+				results.push_back(func(std::forward<Args>(args)...));
 			}
-			return result;
+			return results;
 		}
 	}
 
+	// Utility methods
 	void Clear() { functions.clear(); }
 	bool IsEmpty() const { return functions.empty(); }
 
   private:
-	std::vector<FunctionType> functions;
+	FunctionContainer<FunctionType> functions;
 };
+
+// Alias for std::vector as the default container
+template <typename T> using DelegateVector = Delegate<T, std::vector>;
+
+// Alias for std::list
+template <typename T> using DelegateList = Delegate<T, std::list>;
+
+// Alias for std::deque
+template <typename T> using DelegateDeque = Delegate<T, std::deque>;
